7673461022902d145922982f11bff06ea0523e56
nikitagupta06 <nikitagupta9696@gmail.com>
2023-02-20 00:49:28 +0530

    printing square

--- pattern1.java
+++ pattern1.java
@@ -9,7 +9,6 @@
 class Solution {
 
     void printSquare(int n) {
-        // code here
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("* ");
