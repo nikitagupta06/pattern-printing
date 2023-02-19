fcb5974f01388071da7476c70ad76f51cb04c0e3
nikitagupta06 <nikitagupta9696@gmail.com>
2023-02-20 01:10:59 +0530

    printing diamond

--- pattern9.java
+++ pattern9.java
@@ -15,10 +15,10 @@ class Solution {
 
     void printDiamond(int n) {
         for (int i = 0; i < n; i++) {
-            for (int k = 0; k < n-i-1; k++) {
+            for (int k = 0; k < n - i - 1; k++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < i+1; j++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -27,7 +27,7 @@ class Solution {
             for (int k = 0; k < i; k++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < n-i; j++) {
+            for (int j = 0; j < n - i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
