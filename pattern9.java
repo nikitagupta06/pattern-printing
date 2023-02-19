b4473c9e065b18a931592251423f03b7aab1208b
nikitagupta06 <nikitagupta9696@gmail.com>
2023-02-19 22:33:37 +0530

    pattern 9

--- pattern9.java
+++ pattern9.java
@@ -1,3 +1,36 @@
 /*
-
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
  */
+
+ class Solution {
+
+    void printDiamond(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n-i-1; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
